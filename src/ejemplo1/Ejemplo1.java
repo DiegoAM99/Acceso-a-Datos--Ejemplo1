8f38ba7d80a15c94229401b2719bfff029e1736f
Usuario <Usuario@DESKTOP-PED9DMR>
2018-09-25 12:36:33 +0200

    Va estando

--- src/ejemplo1/Ejemplo1.java
+++ src/ejemplo1/Ejemplo1.java
@@ -5,6 +5,7 @@
  */
 package ejemplo1;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -42,6 +43,7 @@ public class Ejemplo1 {
         System.out.println("------------------------------");
         
     }
+    
     public void EscribeFicheroTextoFileWriter() {
         //Crea el String con la cadena XML
         String texto = "Hola";
@@ -65,4 +67,53 @@ public class Ejemplo1 {
         }
             return -1;
     }
+//    //public void EscribeFicheroTextoFileWriter() {
+///////////////////////Crea el String con la cadena XML
+//                    String texto = "vamos a escribir en el fichero";
+///////////////////////Guarda en la variable nombre el nombre del archivo que se creará.
+//                    String nombre = "pepe.txt";
+//                    try{
+//////////////////////Se crea un Nuevo objeto FileWriter
+//                    FileWriter fichero = new FileWriter ("pepe.txt");
+//////////////////////Se escribe el fichero
+//                    fichero.write(texto + "\r\n");
+//                    while (equals(texto)){
+//                        
+//                    
+//                    
+//                    }
+//////////////////////// Se cierra el fichero
+//                    fichero.close();
+//                    }catch (IOException ex){
+//                    System.out.println("error al acceder al fichero"); 
+//                    }
+//}
+//            public void LeerFicheroTextoFileReader() {
+//////////////////////// se hace un try para poder acceder al fichero guardandolo en un buffer que tiene un strin en blanco.                 
+//                    try{
+//                   FileReader fichero = new FileReader ("pepe.txt");
+//                   BufferedReader miBuffer =new  BufferedReader(fichero);
+//                   String linea=" ";
+////////////////////////  while  para leer fichero almacenandolo en un buffer todo el texto y que valla leyendo mas rapido de linea en linea                
+//                           while(linea!=null){
+//                            linea=miBuffer.readLine();
+//                            
+//                             if(linea!=null)   
+//                            System.out.println(linea);
+//                        }
+////                        fichero.close();
+//                           
+//                           
+//////////////////////// while para leer el fichero de letra en letra el ordenado busca una letra en el fichero y vuelve a si todo el rato                          
+////                        int c=0; 
+////                        while(c!=-1){
+////                            c=fichero.read();
+////                            char leer =(char)c;
+////                            System.out.print(leer);   
+////                        }
+////                        fichero.close();
+// 
+//                    }catch (IOException ex){
+//                    System.out.println("error al acceder al fichero"); }
+//}
 }
